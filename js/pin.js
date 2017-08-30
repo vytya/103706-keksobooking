c98f607a85873f68c6b4a18e98c4713f1957b6d8
vytya <tugo@mail.ru>
2017-08-30 22:32:52 +0300

    UPD

--- js/pin.js
+++ js/pin.js
@@ -1,26 +1,7 @@
 'use strict';
 
 (function () {
-  var pinsBlock = document.querySelector('.tokyo__pin-map');
-  var pinTemplate = document.querySelector('#pin-template').content;
-  var fragment = document.createDocumentFragment();
   var i;
-
-  var renderPin = function (data) {
-    var clonedPinTemplate = pinTemplate.cloneNode(true);
-
-    window.utils.fillData(clonedPinTemplate, '.pin', 'left', data.location.x);
-    window.utils.fillData(clonedPinTemplate, '.pin', 'top', data.location.y);
-    window.utils.fillData(clonedPinTemplate, 'img', 'src', data.author.avatar);
-
-    return clonedPinTemplate;
-  };
-
-  for (i = 0; i < window.data.length; i++) {
-    fragment.appendChild(renderPin(window.data[i]));
-  }
-
-  pinsBlock.appendChild(fragment);
 
   // Pins events
   var pinsList = document.querySelectorAll('.pin:not(.pin__main)');
