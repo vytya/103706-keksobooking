097d9df22ad668acd63b02da7d880eb391019df8
vytya <tugo@mail.ru>
2017-09-01 17:58:01 +0300

    UPD: task 16

--- js/map.js
+++ js/map.js
@@ -6,6 +6,13 @@
     enter: 13
   };
 
+  var MAIN_PIN_MARKER = {
+    width: 75,
+    height: 94
+  };
+
+  var ERROR_BOX_SHADOW = 'inset 0 0 0 2px red';
+
   var i;
 
   window.pin.renderPins(window.data);
@@ -47,6 +54,90 @@
   document.addEventListener('keydown', function (event) {
     if (event.keyCode === KEY_CODES.escape) {
       closePinDialog();
+    }
+  });
+
+  var map = document.querySelector('.tokyo');
+  var mapSize = {
+    width: map.clientWidth,
+    height: map.clientHeight
+  };
+
+  var mainPin = document.querySelector('.pin__main');
+  var addressInput = document.querySelector('#address');
+
+  var pinAddressCoord = {
+    x: mainPin.offsetLeft + MAIN_PIN_MARKER.width / 2,
+    y: mainPin.offsetTop + MAIN_PIN_MARKER.height
+  };
+
+  // Default pin values to address
+  addressInput.value = 'x: ' + pinAddressCoord.x + ', y: ' + pinAddressCoord.y;
+
+  mainPin.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+
+    var startCoords = {
+      x: event.clientX,
+      y: event.clientY
+    };
+
+    var onMouseMove = function (moveEvent) {
+      moveEvent.preventDefault();
+
+      addressInput.style.boxShadow = '';
+
+      var shift = {
+        x: startCoords.x - moveEvent.clientX,
+        y: startCoords.y - moveEvent.clientY
+      };
+
+      startCoords = {
+        x: moveEvent.clientX,
+        y: moveEvent.clientY
+      };
+
+      // Change place of pin
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+
+      // Change address input coords in value
+      pinAddressCoord = {
+        x: mainPin.offsetLeft - shift.x + MAIN_PIN_MARKER.width / 2,
+        y: mainPin.offsetTop - shift.y + MAIN_PIN_MARKER.height
+      };
+
+      addressInput.value = 'x: ' + pinAddressCoord.x + ', y: ' + pinAddressCoord.y;
+    };
+
+    var onMouseUp = function (upEvent) {
+      upEvent.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  addressInput.addEventListener('input', function (event) {
+    var inputArray = event.target.value.replace(',', '').split(' ');
+    var coordX = +inputArray[1];
+    var coordY = +inputArray[3];
+
+    if (typeof (coordX) === 'number' && coordX >= 0 && coordX <= mapSize.width && typeof (coordY) === 'number' && coordY >= 0 && coordY <= mapSize.height) {
+      addressInput.style.boxShadow = '';
+
+      mainPin.style.top = (coordY - MAIN_PIN_MARKER.height) + 'px';
+      mainPin.style.left = (coordX - MAIN_PIN_MARKER.width / 2) + 'px';
+    } else {
+      addressInput.style.boxShadow = ERROR_BOX_SHADOW;
+
+      mainPin.style.top = (mapSize.height - MAIN_PIN_MARKER.height) + 'px';
+      mainPin.style.left = (mapSize.width - MAIN_PIN_MARKER.width / 2) + 'px';
+
+      addressInput.value = 'x: ' + mapSize.width + ', y: ' + mapSize.height;
     }
   });
 }());
