c57eca1da8c2646335b1fd895d9f4dccdcd633c7
vytya <i@vytya.com>
2017-09-08 13:19:09 +0300

    UPD: change filters logic

--- js/filter.js
+++ js/filter.js
@@ -2,32 +2,37 @@
 
 (function () {
   var filters = document.querySelector('.tokyo__filters');
-  var selectFilters = filters.querySelectorAll('select');
-  var featureFilters = filters.querySelectorAll('input[type="checkbox"]');
+  var selectElements = filters.querySelectorAll('select');
+  var featureElements = filters.querySelectorAll('input[type="checkbox"]');
 
   var selectActiveFilters = [];
-  var checkboxActiveValues = [];
+  //var checkboxActiveValues = [];
 
   var updateActiveFilters = function () {
-    window.activeFilters = selectActiveFilters.concat(checkboxActiveValues);
+    window.activeFilters = selectActiveFilters;
+    //window.activeFilters = selectActiveFilters.concat(checkboxActiveValues);
     window.map.updatePins(window.activeFilters);
   };
 
-  [].map.call(selectFilters, function (it, i) {
-    var key = it.name;
-    var filterObject = {};
-    filterObject[key] = it.value;
-
-    selectActiveFilters.push(filterObject);
-
-    it.addEventListener('change', function (event) {
-      selectActiveFilters[i][key] = event.target.value;
-
-      updateActiveFilters();
+  selectActiveFilters = [].map.call(selectElements, function (it, i, array) {
+    it.addEventListener('change', function(event) {
+      changeSelectValues(event);
     });
+
+    return it.value;
   });
 
-  [].map.call(featureFilters, function (it, i) {
+  var changeSelectValues = function (event) {
+    var changedSelectIndex = [].slice.call(selectElements).indexOf(event.target);
+    selectActiveFilters[changedSelectIndex] = event.target.value;
+
+    updateActiveFilters();
+  }
+
+
+  /*
+
+  /*[].map.call(featureFilters, function (it, i) {
     var key = it.value;
     var filterObject = {};
     filterObject[key] = it.checked;
@@ -39,7 +44,7 @@
 
       updateActiveFilters();
     });
-  });
+  });*/
 
   updateActiveFilters();
 }());
